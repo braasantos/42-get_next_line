c66205d2d6bfd347c00680c915d88291bba89049
Braulio Jorge Monteiro Dos Santos <bjorge-m@c1r9s9.42porto.com>
2023-10-17 16:22:35 +0100

    new files

---
+++ get_next_line.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjorge-m <bjorge-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 12:29:54 by bjorge-m          #+#    #+#             */
+/*   Updated: 2023/10/17 13:39:04 by bjorge-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	*mlc;
+	char *get_line;
+
+	str = read(fd, ); // read from a file descriptor
+	//ssize_t read(int fd, void *buff, size_t count); le n bytes (count) indicado pelo buff no fd que vai ser lido
+}
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjorge-m <bjorge-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 12:30:01 by bjorge-m          #+#    #+#             */
+/*   Updated: 2023/10/17 12:32:33 by bjorge-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+#endif
